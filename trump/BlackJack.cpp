2866ae228994b2d67ff4d616e48422c36d9f2451
최윤호 <pizza303@naver.com>
2022-11-27 19:14:48 +0900

    initial commit

---
+++ trump/BlackJack.cpp
@@ -0,0 +1,13 @@
+#include "BlackJack.h"
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+/*void main(array<String^>^ args) {
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	trump::BlackJack form;	// Project1 - 본인 프로젝트 이름
+	Application::Run(% form);
+}
+*/
